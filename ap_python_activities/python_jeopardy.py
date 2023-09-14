9bd245c33788b3a16e1ed4c13732ea3974db4840
rtom1 <130468237+rtom1@users.noreply.github.com>
2023-09-14 14:51:40 +0000

    first commit

---
+++ ap_python_activities/python_jeopardy.py
@@ -0,0 +1,26 @@
+gamers = 1000
+
+playstation = 500
+
+xbox = 300
+
+nintendo = 200 
+
+pc = gamers - playstation - xbox -nintendo
+
+print(pc)
+
+
+
+name = input('whats your name')
+age = input('wahts your age')
+drink = input('can you drink')
+gift = input('what gift are you bringing')
+
+
+pennies = 0
+dimes = 0
+nickels = 0
+quarters = 0
+
+one_dollar= 0
