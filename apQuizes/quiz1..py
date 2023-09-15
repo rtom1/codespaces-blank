3474eabd23f2c978fbff8d06434abb7ccdc8f2a3
rtom1 <130468237+rtom1@users.noreply.github.com>
2023-09-15 14:43:49 +0000

    I'm finshed

--- apQuizes/quiz1..py
+++ apQuizes/quiz1..py
@@ -1,9 +1,15 @@
 # 1. In your on words, describe what a variable is.
+A way of storing information in a program
 
 # 2. Name three (3) operators we've learned about in class and describe 
 # a situation where you would use each one. 
+Arithmetic and you would use it on an online bank program 
+Comparison: x == y
+Activity: Multiply 10 and 3, store result in a variable, and print the result
 
 # 3. Describe the difference between camal case and pascal case.
+thisIsTheCamelCase
+ThisIsThePascalCase
 
 # 4. Which operator is being used in the example below
 numberOfApples = 10
@@ -11,14 +17,21 @@ numberOfOranges = 12
 
 
 numberOfApples < numberOfOranges
+Arithmetic operator
 
 # 5. You have been given a $100.00 dollar gift card from a family member. Another 
 # family member adds 50.00 dollars to your gift card and a friend gives you $35.50. 
 # Your task is to create variables for each gift amount and a final variable that
 # represents the total amount of money on your gift card. 
 
+GiftCard = 100.00
+MA1 = 50
+MA2 = 35.50
+Total = GiftCard + MA1 + MA2
+
 # 6. Create a variable that uses the input function and then print that variable. Your variable 
 # should ask a user to enter their name.  
+Input('What is your name')
 
 # 7. You have been asked to create a program for a community center that wants to plan a cookout. 
 # The community center wants you to build a form program and capture several key data points. 
@@ -33,5 +46,8 @@ numberOfApples < numberOfOranges
 # scenario and how would you write a variable to represent this value?
 
 # 9. In your own words, describe what a program is?
+the process of creating instructions that computers then interpret and follow
 
 # 10. Describe the difference between a float and an integer. 
+Float is a decimal
+Int is a whole number
