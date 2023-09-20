2a2e5e3e9e0f099e4d0e9a2bb590b4842b58a695
rtom1 <130468237+rtom1@users.noreply.github.com>
2023-09-20 15:35:37 +0000

    data classwork

---
+++ ap_python_activities/dataCastingActivity.py
@@ -0,0 +1,25 @@
+# convert the following datatypes into floats.
+12
+100
+
+print(float(12))
+print(float(100))
+
+# convert the following floats into integers.
+12.34
+23.33
+
+print(int(12.34))
+print(int(23.33))
+
+# modify the code below to print an integer number. 
+num_one = 20
+num_two= 23.45
+
+print(int(num_one + num_two))
+
+# modify the following code below to 
+name_of_pet = "Spuny"
+age_of_pet = 10
+
+print("this is my dog "+ name_of_pet + ". He is " + age_of_pet+ " years old.")
