7d22b0f578641de9f6a065853c2615c528a1eb70
rtom1 <130468237+rtom1@users.noreply.github.com>
2023-11-03 14:42:07 +0000

    work

--- ap_python_activities/whileloops.py
+++ ap_python_activities/whileloops.py
@@ -1,3 +1,13 @@
-washer_timer = input('How long do you want to wash your clothes')
-while washer_timer > 0:
-   print('clothes are still washing')
+#washer_timer = input('How long do you want to wash your clothes')
+#hile washer_timer > 0:
+ #  print('clothes are still washing')
+
+def is_valid_password(password):
+    min_length = 8  # Minimum password length
+
+    return len(password) >= min_length
+
+# Example usage:
+password = "Password1"  # Change this to the password you want to check
+valid = is_valid_password(password)
+print("Is the password valid?", valid)
