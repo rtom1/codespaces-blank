cfef85615ea67426bd826ce43a4127bb14e51fec
rtom1 <130468237+rtom1@users.noreply.github.com>
2024-01-23 14:57:20 +0000

    extra credit

---
+++ ap_python_activities/lastPythonUnit/pythonReview.py
@@ -0,0 +1,7 @@
+#1. Telling a computer which task to perform and it running it 
+
+#2. Bool is for true or false situations or like yes or no situations
+  # int is for whole numbers like if you want to count how many people there is 
+  # float is for decimal numbers like money(3.50)
+
+#3. You have to use the input function 
